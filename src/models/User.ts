17a6f49947c3176dd7d47f843260a33ba497b35c
Shlomi Nugarker <shlomin1231@gmail.com>
2025-01-31 12:08:09 +0200

    update user schema

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,12 +1,34 @@
-import mongoose from "mongoose";
+import mongoose, { Schema, Document } from "mongoose";
+import bcrypt from "bcrypt";
 
-const userSchema = new mongoose.Schema(
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  age: number;
+  comparePassword(candidatePassword: string): Promise<boolean>;
+}
+
+const userSchema: Schema<IUser> = new Schema(
   {
     name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    age: { type: Number, required: true },
+    email: { type: String, required: true, unique: true, index: true },
+    password: { type: String, required: true },
   },
   { timestamps: true }
 );
 
-module.exports = mongoose.model("User", userSchema);
+userSchema.pre<IUser>("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+userSchema.methods.comparePassword = async function (
+  candidatePassword: string
+): Promise<boolean> {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+export const User = mongoose.model<IUser>("User", userSchema);
